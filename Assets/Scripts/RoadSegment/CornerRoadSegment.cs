35243566f5e547a708796d268a66be157aca6a8f
Jin Xu <xujin@i-vsun.com>
2016-07-08 14:25:33 +0800

    fixed roll error

--- Assets/Scripts/RoadSegment/CornerRoadSegment.cs
+++ Assets/Scripts/RoadSegment/CornerRoadSegment.cs
@@ -23,14 +23,6 @@ public class CornerRoadSegment : BaseRoadSegment
 
     public CornerRoadSegment(RoadPoint pointA, float roadWidth, float pitch, float roll, float radius, float angle) : base(pointA, roadWidth)
     {
-        if (angle > 0)
-        {
-
-        }
-        else
-        {
-
-        }
         this.pitch = pitch;
         this.yaw = pointA.yaw;
         this.roll = roll;
@@ -42,7 +34,7 @@ public class CornerRoadSegment : BaseRoadSegment
         base.pointA.roll = roll;
 
         //rotation
-        rotation = Quaternion.Euler(pitch, yaw, roll);
+        rotation = Quaternion.Euler(pitch, yaw, 0); //roll 不进行旋转，roll只是用于决定三角形表面角度
 
         //calculate point B
         Vector3 direction = new Vector3(Mathf.Sign(angle), 0, 0);
@@ -50,12 +42,23 @@ public class CornerRoadSegment : BaseRoadSegment
 
         //rotate pointA around circleCenter to pointB 
         Vector3 upwards = rotation * Vector3.up;
+        Vector3 downwards = rotation * Vector3.down;
+
         Quaternion pointBRotation = Quaternion.AngleAxis(angle, upwards);
         Vector3 bPosition = circleCenter + pointBRotation * ((pointA.position - circleCenter).normalized) * radius;
 
         //
         Vector3 centerToB = bPosition - circleCenter;
-        Vector3 bForward = Vector3.Cross(upwards, centerToB);
+        Vector3 bForward;
+
+        if (angle < 0)
+        {
+            bForward = Vector3.Cross(downwards, centerToB);
+        }
+        else
+        {
+            bForward = Vector3.Cross(upwards, centerToB);
+        }
 
         Quaternion bRotation = Quaternion.LookRotation(bForward, upwards);
 
@@ -67,10 +70,9 @@ public class CornerRoadSegment : BaseRoadSegment
         //length
         roadLength = 2 * Mathf.PI * radius * Mathf.Abs(angle) / 360;
 
-        Debug.DrawLine(pointA.position, circleCenter, Color.red, 20);
-        Debug.DrawLine(pointB.position, circleCenter, Color.red, 20);
+        Debug.DrawLine(pointA.position, circleCenter, Color.red, 100);
+        Debug.DrawLine(pointB.position, circleCenter, Color.red, 100);
         Debug.Log("Corner segment PointB  pos: " + bPosition + ", pitch: " + bPitch + ", yaw: " + bYaw + ", roll: " + bRoll);
-
     }
 
     protected override void UpdateValues()
@@ -89,7 +91,7 @@ public class CornerRoadSegment : BaseRoadSegment
         Vector3 direction = new Vector3(Mathf.Sign(angle), 0, 0);
         Vector3 circleCenter = pointA.position + rotation * direction * radius;
         Vector3 upwards = rotation * Vector3.up;
-
+        Vector3 downwards = rotation * Vector3.down;
         //
         List<Vector3> vertices = new List<Vector3>();
         List<int> triangles = new List<int>();
@@ -103,11 +105,23 @@ public class CornerRoadSegment : BaseRoadSegment
         {
             Quaternion angleRotation = Quaternion.AngleAxis(i * angleIncrement, upwards);
             Vector3 position = circleCenter + angleRotation * ((pointA.position - circleCenter).normalized) * radius;
-            Vector3 pointForward = Vector3.Cross(upwards, position - circleCenter);
-            Quaternion pointRotation = Quaternion.LookRotation(pointForward, upwards);
 
-            Vector3 left = position + pointRotation * Vector3.left;
-            Vector3 right = position + pointRotation * Vector3.right;
+            Vector3 pointUpwards = angleRotation * Quaternion.Euler(0, 0, roll) * Vector3.up;
+            Vector3 pointDownwards = angleRotation * Quaternion.Euler(0, 0, roll) * Vector3.down;
+
+            Vector3 pointForward;
+            if (angle < 0)
+            {
+                pointForward = Vector3.Cross(pointDownwards, position - circleCenter);
+            }
+            else
+            {
+                pointForward = Vector3.Cross(pointUpwards, position - circleCenter);
+            }
+
+            Quaternion pointRotation = Quaternion.LookRotation(pointForward, pointUpwards);
+            Vector3 left = position + pointRotation * Vector3.left * halfWidth;
+            Vector3 right = position + pointRotation * Vector3.right * halfWidth;
 
             vertices.Add(left);
             vertices.Add(right);
@@ -134,3 +148,160 @@ public class CornerRoadSegment : BaseRoadSegment
         return data;
     }
 }
+
+
+/*
+version 1
+
+    using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// 转角路段
+/// </summary>
+public class CornerRoadSegment : BaseRoadSegment
+{
+    public float yaw;
+
+    public float pitch;
+
+    public float roll;
+
+    public Quaternion rotation;
+
+
+    public float radius;
+
+    public float angle;
+
+    public CornerRoadSegment(RoadPoint pointA, float roadWidth, float pitch, float roll, float radius, float angle) : base(pointA, roadWidth)
+    {
+        this.pitch = pitch;
+        this.yaw = pointA.yaw;
+        this.roll = roll;
+        this.radius = radius;
+        this.angle = angle;
+
+        //point A
+        base.pointA.pitch = pitch;
+        base.pointA.roll = roll;
+
+        //rotation
+        rotation = Quaternion.Euler(pitch, yaw, roll);
+
+        //calculate point B
+        Vector3 direction = new Vector3(Mathf.Sign(angle), 0, 0);
+        Vector3 circleCenter = pointA.position + rotation * direction * radius;
+
+        //rotate pointA around circleCenter to pointB 
+        Vector3 upwards = rotation * Vector3.up;
+        Vector3 downwards = rotation * Vector3.down;
+
+
+
+        Quaternion pointBRotation = Quaternion.AngleAxis(angle, Vector3.up);
+        Vector3 bPosition = circleCenter + pointBRotation * ((pointA.position - circleCenter).normalized) * radius;
+
+        //
+        Vector3 centerToB = bPosition - circleCenter;
+        Vector3 bForward;
+
+        if (angle < 0)
+        {
+            bForward = Vector3.Cross(downwards, centerToB);
+        }
+        else
+        {
+            bForward = Vector3.Cross(upwards, centerToB);
+        }
+
+        Quaternion bRotation = Quaternion.LookRotation(bForward, upwards);
+
+        float bPitch = bRotation.eulerAngles.x;
+        float bYaw = bRotation.eulerAngles.y;
+        float bRoll = bRotation.eulerAngles.z;
+
+        pointB = new RoadPoint(bPosition, bPitch, bYaw, bRoll);
+        //length
+        roadLength = 2 * Mathf.PI * radius * Mathf.Abs(angle) / 360;
+
+        Debug.DrawLine(pointA.position, circleCenter, Color.red, 100);
+        Debug.DrawLine(pointB.position, circleCenter, Color.red, 100);
+        Debug.Log("Corner segment PointB  pos: " + bPosition + ", pitch: " + bPitch + ", yaw: " + bYaw + ", roll: " + bRoll);
+    }
+
+    protected override void UpdateValues()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override float getY(float x, float z)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override MeshData GenerateMesh(int subdivision, int baseIndex)
+    {
+        //helper variables
+        Vector3 direction = new Vector3(Mathf.Sign(angle), 0, 0);
+        Vector3 circleCenter = pointA.position + rotation * direction * radius;
+        Vector3 upwards = rotation * Vector3.up;
+        Vector3 downwards = rotation * Vector3.down;
+        //
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+
+        float halfWidth = roadWidth / 2;
+
+        float angleIncrement = angle / subdivision;
+
+        //add vertices
+        for (int i = 0; i <= subdivision; ++i)
+        {
+            Quaternion angleRotation = Quaternion.AngleAxis(i * angleIncrement, upwards);
+            Vector3 position = circleCenter + angleRotation * ((pointA.position - circleCenter).normalized) * radius;
+
+            Vector3 pointForward;
+            if (angle < 0)
+            {
+                pointForward = Vector3.Cross(downwards, position - circleCenter);
+            }
+            else
+            {
+                pointForward = Vector3.Cross(upwards, position - circleCenter);
+            }
+
+            Quaternion pointRotation = Quaternion.LookRotation(pointForward, upwards);
+            Vector3 left = position + pointRotation * Vector3.left * halfWidth;
+            Vector3 right = position + pointRotation * Vector3.right * halfWidth;
+
+            vertices.Add(left);
+            vertices.Add(right);
+        }
+
+        //add triangles
+        for (int i = 0; i < subdivision; ++i)
+        {
+            //triangle left
+            triangles.Add(baseIndex + (2 * (i + 1)));
+            triangles.Add(baseIndex + (2 * i + 1));
+            triangles.Add(baseIndex + (2 * i));
+
+            //triangles right
+            triangles.Add(baseIndex + (2 * (i + 1)));
+            triangles.Add(baseIndex + (2 * (i + 1) + 1));
+            triangles.Add(baseIndex + (2 * i + 1));
+        }
+
+        MeshData data = new MeshData();
+        data.vertices = vertices;
+        data.triangles = triangles;
+
+        return data;
+    }
+}
+
+
+    */
