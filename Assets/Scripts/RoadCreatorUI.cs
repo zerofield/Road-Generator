04cf9c9428225a9a0b8372e9e80d2d99e4ecb77d
Jin <zerofield@outlook.com>
2016-07-12 07:41:55 +0800

    removed deprecated files

--- Assets/Scripts/RoadCreatorUI.cs
+++ Assets/Scripts/RoadCreatorUI.cs
@@ -90,57 +90,55 @@ public class RoadCreatorUI : MonoBehaviour
     public void OnAddSegmentClicked()
     {
         float width = tryGetFloat(widthInputField.text, minWidth);
-
         float length = tryGetFloat(lengthInputField.text, minLength);
         float pitch = tryGetFloat(pitchInputField.text, 0);
         float roll = tryGetFloat(rollInputField.text, 0);
         float angle = tryGetFloat(angleInputField.text, 0);
         float radius = tryGetFloat(radiusInputField.text, 0);
-
         int subdivision = (int)tryGetFloat(subdivistionField.text, minSubdivition);
 
-        RoadPoint pointA;
+        //RoadPoint pointA;
 
-        int count = creator.RawSegmentsCount;
-        BaseRoadSegment segment = null;
+        //int count = creator.RawSegmentsCount;
+        //BaseRoadSegment segment = null;
 
-        if (count == 0)
-        {
-            float tempPitch = 0;
-            float tempYaw = 0;
-            float tempRoll = 0;
-            pointA = new RoadPoint(Vector3.zero, tempPitch, tempYaw, tempRoll);
-        }
-        else
-        {
-            pointA = creator.LastRoadSegment().pointB;
-        }
+        //if (count == 0)
+        //{
+        //    float tempPitch = 0;
+        //    float tempYaw = 0;
+        //    float tempRoll = 0;
+        //    pointA = new RoadPoint(Vector3.zero, tempPitch, tempYaw, tempRoll);
+        //}
+        //else
+        //{
+        //    pointA = creator.LastRoadSegment().pointB;
+        //}
 
-        //分为转弯和平面两个情况来考虑
-        if (angle != 0 && radius > 0)    //转弯路面
-        {
-            segment = new CornerRoadSegment(pointA, width, pitch, roll, radius, angle);
-        }
-        else //普通路面
-        {
-            segment = new SimpleRoadSegment(pointA, width, length, pitch, roll);
-        }
+        ////分为转弯和平面两个情况来考虑
+        //if (angle != 0 && radius > 0)    //转弯路面
+        //{
+        //    segment = new CornerRoadSegment(pointA, width, pitch, roll, radius, angle);
+        //}
+        //else //普通路面
+        //{
+        //    segment = new SimpleRoadSegment(pointA, width, length, pitch, roll);
+        //}
 
-        creator.AddSegment(segment);
-        creator.GenerateMesh(0, subdivision, false);
+        //creator.AddSegment(segment);
+        //creator.GenerateMesh(0, subdivision, false);
 
-        removeSegmentButton.interactable = true;
+        //removeSegmentButton.interactable = true;
     }
 
     public void OnRemoveSegmentClicked()
     {
-        creator.RemoveLastSegment();
-        if (creator.RawSegmentsCount <= 0)
-        {
-            removeSegmentButton.interactable = false;
-        }
-        int subdivision = (int)tryGetFloat(subdivistionField.text, minSubdivition);
-        creator.GenerateMesh(0, subdivision, false);
+        //creator.RemoveLastSegment();
+        //if (creator.RawSegmentsCount <= 0)
+        //{
+        //    removeSegmentButton.interactable = false;
+        //}
+        //int subdivision = (int)tryGetFloat(subdivistionField.text, minSubdivition);
+        //creator.GenerateMesh(0, subdivision, false);
     }
 
     public void OnSubdivisionChanged(string newText)
--- Assets/Scripts/RoadMeshCreator.cs
+++ Assets/Scripts/RoadMeshCreator.cs
@@ -11,15 +11,9 @@ public class RoadMeshCreator : MonoBehaviour
 
     private MeshFilter meshFilter;
     private MeshRenderer meshRenderer;
-    private List<BaseRoadSegment> rawSegments = new List<BaseRoadSegment>();
 
-    public int RawSegmentsCount
-    {
-        get
-        {
-            return rawSegments.Count;
-        }
-    }
+
+    private SegmentNode currentNode;
 
     void Awake()
     {
@@ -27,35 +21,29 @@ public class RoadMeshCreator : MonoBehaviour
         meshRenderer = GetComponent<MeshRenderer>();
     }
 
-    public void AddSegment(BaseRoadSegment segment)
+    public void AddSegment(SegmentNode newNode)
     {
-        if (segment != null)
+        if (currentNode != null)
         {
-            rawSegments.Add(segment);
+            currentNode.AddNode(newNode);
+            currentNode = newNode;
+        }
+        else
+        {
+            currentNode = newNode;
         }
     }
 
     public void RemoveLastSegment()
     {
-        if (rawSegments.Count > 0)
+        if (currentNode != null)
         {
-            rawSegments.RemoveAt(rawSegments.Count - 1);
+            currentNode = currentNode.parent;
+            if (currentNode != null)
+            {
+            }
         }
     }
-
-    /// <summary>
-    /// 获得最后一个路段
-    /// </summary>
-    /// <returns></returns>
-    public BaseRoadSegment LastRoadSegment()
-    {
-        if (rawSegments.Count == 0)
-        {
-            return null;
-        }
-        return rawSegments[rawSegments.Count - 1];
-    }
-
 
     /// <summary>
     /// 生成路面网格模型
@@ -68,60 +56,10 @@ public class RoadMeshCreator : MonoBehaviour
         smoothPercent = Mathf.Clamp(smoothPercent, 0, 0.5f);
         subdivision = Mathf.Clamp(subdivision, 2, int.MaxValue);
 
-        Mesh mesh = new Mesh();
-
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> triangles = new List<int>();
-
-        List<BaseRoadSegment> segments = rawSegments;
-        if (smooth)
-        {
-            //平滑路段，使得路面连续
-            segments = smoothSegments(rawSegments);
-        }
-
-        if (segments != null)
-        {
-            foreach (BaseRoadSegment segment in segments)
-            {
-                int baseIndex = vertices.Count;
-                BaseRoadSegment.MeshData meshData = segment.GenerateMesh(subdivision, baseIndex);
-                vertices.AddRange(meshData.vertices);
-                triangles.AddRange(meshData.triangles);
-            }
-        }
-
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
-        mesh.Optimize();
-        meshFilter.mesh = mesh;
+       
     }
 
-    /// <summary>
-    /// 平滑路段
-    /// </summary>
-    /// <returns></returns>
-    private List<BaseRoadSegment> smoothSegments(List<BaseRoadSegment> rawSegments)
-    {
-        //TODO 
 
-        List<BaseRoadSegment> newSegments = new List<BaseRoadSegment>();
-
-        for (int i = 0; i < rawSegments.Count - 1; ++i)
-        {
-            BaseRoadSegment segment1 = rawSegments[i];
-            BaseRoadSegment segment2 = rawSegments[i + 1];
-
-           
-
-
-        }
-
-
-        return newSegments;
-    }
 
 
 }
